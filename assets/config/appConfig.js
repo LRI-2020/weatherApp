8f38dfd344bf80f444815765f9519cf8443a1824
LaylaR <layla.richard.2111@gmail.com>
2023-11-13 16:51:43 +0100

    tryEnvVar

--- assets/config/appConfig.js
+++ assets/config/appConfig.js
@@ -1,9 +1,10 @@
 ﻿let geoCodingHost = "https://api.openweathermap.org/geo/1.0/";
 let OpenWeatherHost ="https://api.openweathermap.org/data/2.5/forecast?";
-let apiKey = await fetch("./assets/Secrets/secrets.json")
-    .then((res) => res.json())
-    .then((data) => data.weatherAppApiKey)
-    .catch((e) => e);
+let apiKey = process.env.weather_App_Api_Key; 
+// await fetch("./assets/Secrets/secrets.json")
+//     .then((res) => res.json())
+//     .then((data) => data.weatherAppApiKey)
+//     .catch((e) => e);
 
 function getIconUrl(iconCode,size){
     return `https://openweathermap.org/img/wn/${iconCode}${size}.png`;
